7c41d95d04e3f4d914ec44323c569942b360e7be
knouz15 <benknouz@yahoo.fr>
2022-08-29 09:54:20 +0200

    modifs

---
+++ src/Controller/PhoneController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Phone;
+use App\Repository\PhoneRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class PhoneController extends AbstractController
+{
+    #[Route('/api/phones', name: 'phones', methods: ['GET'])]
+    public function getPhoneList(
+        PhoneRepository $phoneRepository,
+        SerializerInterface $serializer
+    ): JsonResponse
+    {
+        $phoneList = $phoneRepository->findAll();
+        $jsonPhoneList = $serializer->serialize($phoneList, 'json');
+        return new JsonResponse($jsonPhoneList, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/phones/{id}', name: 'detailPhone', methods: ['GET'])]
+    public function getDetailPhone(Phone $phone, SerializerInterface $serializer): JsonResponse {
+
+        $jsonPhone = $serializer->serialize($phone, 'json');
+        return new JsonResponse($jsonPhone, Response::HTTP_OK, ['accept' => 'json'], true);
+   }
+}
